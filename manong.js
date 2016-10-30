3799f2b8333825e06284062bd02b39380ed7b7bc
Qin Xiang <rupert.qing@gmail.com>
2016-10-31 02:37:09 +0800

    use try .. catch to handle promisify error

--- manong.js
+++ manong.js
@@ -1,10 +1,15 @@
-// use flag: --harmony_async_await
+// use flag: --harmony_async_await 
+// node version >= 7.0
 const request = require('request')
 const async = require('async')
 const cheerio = require('cheerio')
 const AV = require('leancloud-storage')
 var read = require('node-readability')
 const querystring = require('querystring')
+const Promise = require('bluebird')
+
+const requestPromise = Promise.promisify(request, {multiArgs: false})
+const readPromise = Promise.promisify(read, {multiArgs: false})
 
 const baseUrlQA = 'http://weekly.manong.io/issues/'
 let myConfig = {
@@ -49,33 +54,32 @@ class Crawler {
                 'User-Agent': `Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36 115Browser/6.0.3`,
             }
         };
-
-        var {error, response, body} = await this.requestPromise(options)
-        console.log(response.request.href)
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(body) 
+        
+        var data = await requestPromise(options)
+        if (data.statusCode == 200) {
+            const $ = cheerio.load(data.body) 
             async.eachOfSeries($('h4'), (el, i, callback2)=> {
                 let ATag = $(el).find('a').first()
                 let link = querystring.parse($(ATag).attr('href').split('?')[1]).url
                 // link = decodeURIComponent(link)
-                let data = {
+                let obj = {
                     title: $(ATag).text(),
                     link: link,
                 }
                 console.log('link: ', link)
-                this.getDetail(link, data, callback2)
+                this.getDetail(link, obj, callback2)
             }, ()=> {
                 console.log('list ' + url + ' is saved!!!')
                 callback(null)
             })
         } else {
-            console.log('========= bad href: ', response.request.href)
+            console.log('bad href: ', data.request.href)
             // continue
             callback(null)
         }
     }
 
-    async getDetail(url, data, callback2) {
+    async getDetail(url, obj, callback2) {
         const options = {
             url: url,
             timeout: 5 * 1000,
@@ -83,33 +87,47 @@ class Crawler {
                 'User-Agent': `Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.85 Safari/537.36 115Browser/6.0.3`,
             }
         };
-        var {error, response, body} = await this.requestPromise(options)
-        if (!error && response.statusCode == 200) {
-            read(body, (err, article, meta)=> {
+        let data
+        try {
+            data = await requestPromise(options)
+            if (data && data.statusCode == 200) {
+                const article = await readPromise(data.body)
                 if (!article) return callback2(null)
-                let data = {}
-                data.url = url
-                data.title = article.title
-                data.content = article.content
-                this.save(data, callback2)
+                this.save({
+                    url: url,
+                    title: article.title,
+                    content: article.content
+                }, callback2)
                 article.close();
-            })
-        } else {
-            if (response) {
-                console.log('========= bad href: ', response.request.href)
+            } else {
+                console.log('bad href: ', url)
+                // continue
+                callback2(null)
             }
-            // continue
-            callback2(null)
+        } catch (err){
+            if (err) {
+                console.log('bad href2: ', url)
+                callback2(null)
+            }
         }
+        
     }
 
-    requestPromise(options) {
-        return new Promise(function(resolve, reject) {
-            request(options, function(error, response, body) {
-                resolve({error, response, body})
-            })
-        })
-    }
+    // requestPromise(options) {
+    //     return new Promise(function(resolve, reject) {
+    //         request(options, function(error, response, body) {
+    //             resolve({error, response, body})
+    //         })
+    //     })
+    // }
+
+    // readPromise(body) {
+    //     return new Promise(function(resolve, reject) {
+    //         read(body, function(err, article, meta) {
+    //             resolve({err, article, meta})
+    //         })
+    //     })
+    // }
 
     save(data, callback2) {
         const MaNong = AV.Object.extend('MaNong2');
